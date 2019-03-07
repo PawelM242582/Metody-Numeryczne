ec87fd8c2525d99789c55ce50b8dd0c19118d2a1
PawelM242582 <pawelekm2000@gmail.com>
2019-03-07 08:49:51 +0100

    index

--- ex3.py
+++ ex3.py
@@ -1,4 +1,5 @@
 #3 write a function which takes an array of numbers as an input and finds the lowest value. Return the index of that element and its value (1p)
+import numpy as np
 def minimum(x):
    z = min(x)
    y = x.index(min(x))
@@ -9,7 +10,12 @@ while True :
         array=list(map(int,input("podaj liczby oddzielone spacjami").split()))
         print(array)
         print("minimum to " , minimum(array)[0])
-        print("index minimum to " , minimum(array)[1] )
+        x = minimum(array)[0]
+        index = []
+        for index in range(len(array)):
+           if array[index] == x:
+               print("index minimum to : " , index)
+
         break
     except ValueError:
         print("musisz podać liczbę")
