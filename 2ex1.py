23cbd7f8de2b492100ab1db8fc37390bff23632c
PawelM242582 <pawelekm2000@gmail.com>
2019-03-12 17:02:21 +0100

    changed int into flat

--- 2ex1.py
+++ 2ex1.py
@@ -1,6 +1,6 @@
 #1 Perimeter & field of circles with given radius X for the first circle & Y for the second one. (1p)
 from math import pi
-from cs50 import get_int
+from cs50 import get_float
 
 def perimeter(x):
     return 2* pi *x
@@ -8,8 +8,8 @@ def perimeter(x):
 def field(x):
     return pi * x * x
 
-x = get_int("podaj x")
-y = get_int("podaj y")
+x = get_float("podaj x") 
+y = get_float("podaj y")
 
 print("Perimeter for %s is %s " %(x, perimeter(x)))
 print("field for %s is %s " %(x, perimeter(x)))
