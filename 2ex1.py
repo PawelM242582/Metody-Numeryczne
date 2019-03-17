c1700e9af0e69e38438b91cf9adba40ee23db542
PawelM242582 <pawelekm2000@gmail.com>
2019-03-17 18:04:49 +0100

    add printing field instead of perimeter twice

--- 2ex1.py
+++ 2ex1.py
@@ -3,16 +3,18 @@ from math import pi
 from cs50 import get_float
 
 def perimeter(x):
-    return 2* pi *x
+    return "{0:.2f}".format(2* pi *x)
 
 def field(x):
-    return pi * x * x
+    return "{0:.2f}".format(pi * x**2)
 
 x = get_float("podaj x ") 
 y = get_float("podaj y ")
 
 print("Perimeter for %s is %s " %(x, perimeter(x)))
-print("field for %s is %s " %(x, perimeter(x)))
+print("field for %s is %s " %(x, field(x)))
 print("Perimeter for %s is %s " %(y, perimeter(y)))
-print("field for %s is %s " %(y, perimeter(y)))
+print("field for %s is %s " %(y, field(y)))
+
+#dodajć printa "podaj poprawną wartość"
 
